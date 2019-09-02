eaa6418cf4a3da6c6b4fd6cd4ba79bfd5d9bc143
Nurudeen <nurudeen.amedu@gmail.com>
2019-09-02 13:12:27 +0100

    selectors created

--- js/index.js
+++ js/index.js
@@ -40,3 +40,26 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let nav = document.getElementsByTagName('a');
+let cta = document.getElementsByClassName('cta-text');
+let ctaImage = document.getElementById("cta-img");
+let mainContent = document.querySelector('.main-content div:nth-child(1) h4');
+let mainContentText = document.querySelector('.main-content div:nth-child(1) p');
+let mainContent2 = document.querySelector('.main-content div:nth-child(2) h4');
+let mainContentText2 = document.querySelector('.main-content div:nth-child(2) p');
+let mainImage = document.querySelector('.main-content img');
+let bottomContent = document.querySelector('.bottom-content div:nth-child(1) h4');
+let bottomContentText = document.querySelector('.bottom-content div:nth-child(1) p');
+let bottomContent2 = document.querySelector('.bottom-content div:nth-child(2) h4');
+let bottomContentText2 = document.querySelector('.bottom-content div:nth-child(2) p');
+let bottomContent3 = document.querySelector('.bottom-content div:nth-child(3) h4');
+let bottomContentText3 = document.querySelector('.bottom-content div:nth-child(3) p');
+
+let contactH4 = document.querySelector('.contact h4');
+let contactP1 = document.querySelector('.contact p:nth-child(2)');
+let contactP2 = document.querySelector('.contact p:nth-child(3)');
+let contactP3 = document.querySelector('.contact p:nth-child(4)');
+
+let footerP = document.querySelector('footer p');
+
